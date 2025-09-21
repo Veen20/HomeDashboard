839d62c263be9ee5a4b47aa6efa890b50ee12831
Veen20 <150326542+Veen20@users.noreply.github.com>
2025-09-21 19:31:54 +0700

    Update home.py

--- home.py
+++ home.py
@@ -135,7 +135,7 @@ if st.sidebar.button("ℹ️ Tentang"):
 st.markdown("""
 <div class="top-right-logos">
     <img src="logo unsri.png">
-    <img src="logo samsat.png">
+    <img src="https://github.com/Veen20/HomeDashboard/blob/main/logo%20samsat.png">
     <img src="logo fasilkom.png">
         
 </div>
